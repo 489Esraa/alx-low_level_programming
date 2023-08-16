a23cb675e63fea6921286a221e15452ad17c9d6b
489Esraa <asraaalaa2050@gmail.com>
2023-08-15 18:42:16 -0700

    edrtfg

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - this function print all alphabet
+ *
+*/
+void print_alphabet(void)
+{
+int character;
+
+for (character = 'a'; character <= 'z'; character++)
+_putchar(character);
+_putchar('\n');
+}
