a83cb60e7f8c13e5690ecffc628509b54a7cb19f
489Esraa <asraaalaa2050@gmail.com>
2023-08-18 15:23:34 -0700

    hgfd

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,7 +3,7 @@
 /**
  * main - check the code
  *
- * Description: prints the numbers, from 0 to 9, followed by a new line.	
+ * Description: prints the numbers, from 0 to 9, followed by a new line.
  *
  * Return: Always 0.
 */
@@ -11,13 +11,10 @@ void print_most_numbers(void)
 {
 char c;
 
-for (c = 0;c < 9; c++)
-{
 for (c = '0'; c <= '9'; c++)
 {
 if (!(c == '2' || c == '4'))
 _putchar(c);
 }
 _putchar('\n');
-}
 }
