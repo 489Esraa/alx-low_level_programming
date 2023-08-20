f7b91117796199c4890341349718095f08b0aef9
489Esraa <asraaalaa2050@gmail.com>
2023-08-20 08:52:49 -0700

    bvc

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -17,7 +17,7 @@ for (i = n; i >= 98; i--)
 if (i != 98)
 printf("%d, ", i);
 else if (i == 98)
-printf("%d,\n", i);
+printf("%d\n", i);
 }
 }
 else if (n <= 98)
@@ -27,7 +27,7 @@ for (k = n; k <= 98; k++)
 if (k != 98)
 printf("%d, ", k);
 else if (k == 98)
-printf("%d,\n", k);
+printf("%d\n", k);
 }
 }
 }
