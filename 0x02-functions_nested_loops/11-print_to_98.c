51fa441687f25eabcf5f4e67e8dd7a65b95e718b
489Esraa <asraaalaa2050@gmail.com>
2023-08-20 01:44:02 -0700

    tfrd

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,9 +8,11 @@
 */
 void print_to_98(int n)
 {
+int i, k;	
+
 if (n >= 98)
 {
-for (int i = n; i <= 98; i--)
+for (i = n; i <= 98; i--)
 {
 if (i != 98)
 printf("%d, ", i);
@@ -20,12 +22,12 @@ printf("%d,\n", i);
 }
 else if (n <= 98)
 {
-for (int i = n; i <= 98; i++)
+for (k = n; k <= 98; k++)
 {
-if (i != 98)
-printf("%d, ", i);
-else if (i == 98)
-printf("%d,\n", i);
+if (k != 98)
+printf("%d, ", k);
+else if (k == 98)
+printf("%d,\n", k);
 }
 }
 }
