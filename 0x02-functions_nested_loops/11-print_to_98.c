9a5b0004af7c0e4bb7844c259a2e69293ea1e046
489Esraa <asraaalaa2050@gmail.com>
2023-08-20 01:58:46 -0700

    tfrd

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,20 +5,23 @@
  * print_to_98: numbers from n to 98, followed by a new line.
  *
  * @n: int to count to 98
- * follwed by new line
 */
 void print_to_98(int n)
 {
-int i, k;	
+int i, k;
 
 if (n >= 98)
 {
 for (i = n; i <= 98; i--)
 {
 if (i != 98)
+{
 printf("%d, ", i);
+}
 else if (i == 98)
+{
 printf("%d,\n", i);
+}
 }
 }
 else if (n <= 98)
@@ -26,9 +29,13 @@ else if (n <= 98)
 for (k = n; k <= 98; k++)
 {
 if (k != 98)
+{
 printf("%d, ", k);
+}
 else if (k == 98)
+{
 printf("%d,\n", k);
+}
 }
 }
 }
