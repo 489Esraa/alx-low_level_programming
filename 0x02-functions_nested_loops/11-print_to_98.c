721c1543faf97983c9337612cb63186b5b7a5b96
489Esraa <asraaalaa2050@gmail.com>
2023-08-20 02:03:58 -0700

    tfrd

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,13 +15,9 @@ if (n >= 98)
 for (i = n; i <= 98; i--)
 {
 if (i != 98)
-{
 printf("%d, ", i);
-}
 else if (i == 98)
-{
 printf("%d,\n", i);
-}
 }
 }
 else if (n <= 98)
@@ -29,13 +25,9 @@ else if (n <= 98)
 for (k = n; k <= 98; k++)
 {
 if (k != 98)
-{
 printf("%d, ", k);
-}
 else if (k == 98)
-{
 printf("%d,\n", k);
-}
 }
 }
 }
