cf679e4a4387a8f76fb7e109bb80438a4b5f2dda
489Esraa <asraaalaa2050@gmail.com>
2023-10-04 08:27:21 -0700

    esraa

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+*/
+int _strlen(char *s)
+{
+int longi = 0;
+
+while (*s != '\0')
+{
+longi++;
+s++;
+}
+return (longi);
+}
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter
+ * @str : size of malloc
+ * Return: pointer to the array intialized or null
+*/
+char *_strdup(char *str)
+{
+int i;
+int size = _strlen(str);
+
+char *ptr = (char *)malloc((size + 1) * sizeof(char));
+for (i = 0; i <= size; i++)
+{
+*(ptr + i) = str[i];
+}
+return (ptr);
+}
