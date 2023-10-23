79724e70562abca3514d650dd0a5a7d5acc3d342
489Esraa <asraaalaa2050@gmail.com>
2023-10-23 01:05:22 -0700

    esraaalaa

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,16 +1,17 @@
 #include "holberton.h"
+#include "stdlib.h"
 
 /**
- * malloc_checked - allocates memory using malloc
+ * malloc_checked - calls exit on failure
  * @b: number of bytes to allocate
- * Return: a pointer to the allocated memory
+ *
+ * Return: void
  */
 void *malloc_checked(unsigned int b)
 {
-	void *ptrtomem;
+	void *p = malloc(b);
 
-	ptrtomem = malloc(b);
-	if (ptrtomem == NULL)
+	if (!p)
 		exit(98);
-	return (ptrtomem);
+	return (p);
 }
