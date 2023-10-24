5c2c069a78603dd292628ea7b527147913bcb775
489Esraa <asraaalaa2050@gmail.com>
2023-10-24 15:30:32 -0700

    esraaalaa

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,41 +1,40 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <main.h>
+#include "main.h"
 
 /**
- * alloc_grid - returns a pointer to a 2 dimensional array of integers.
- * @width: width of array
- * @height: height of array
- * Return: a pointer to a 2 dimensional array of integers.
-*/
+ * **alloc_grid - returns a pointer to a 2 dimensional array of integers.
+ * @width: integer parameter
+ * @height: integer parameter
+ * Return:  2d array
+ */
+
 int **alloc_grid(int width, int height)
 {
-int **rows;
-int x, j;
 
-if (width <= 0 || height <= 0)
-return (NULL);
+	int **tab, i, j;
 
-rows = (int **)malloc(wight * sizeof(int *));
-if (rows == NULL)
-return (NULL);
-for (x = 0; x < width; x++)
-{
-rows[x] = (int *)malloc(height * sizeof(int));
-if (!rows)
-{
-for (j = 0; j < x; j++)
-{
-free([j]);
-}
-free(rows);
-return (NULL);
-}
-}
-}
-for (j = 0; j < height; j++){
-for (x = 0; x < width; x++)
-rows[j][x] = 0;
-}
-return (rows);
+	tab = malloc(sizeof(*tab) * height);
+
+	if (width <= 0 || height <= 0 || tab == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		for (i = 0; i < height; i++)
+		{
+			tab[i] = malloc(sizeof(**tab) * width);
+			if (tab[i] == 0)
+			{
+				while (i--)
+					free(tab[i]);
+				free(tab);
+				return (0);
+			}
+
+			for (j = 0; j < width; j++)
+				tab[i][j] = 0;
+		}
+	}
+
+	return (tab);
 }
