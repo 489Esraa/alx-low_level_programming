d7062e11c842eb6a3baa212d6b15a40168551f1c
489Esraa <asraaalaa2050@gmail.com>
2024-05-07 13:43:58 +0300

    esraaalaa

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -18,7 +18,7 @@ int binary_search(int *array, size_t size, int value){
     {
         for(i = l; i <= h; i++)
         {
-            printf("Searching in array: array[ld]", array[i]);
+            printf("Searching in array: [d]", array[i]);
             if(i != h)
                 printf(", ");
             else
