44905014e86a4fa3354a0b7f1916084469f651e3
489Esraa <asraaalaa2050@gmail.com>
2024-05-07 13:39:55 +0300

    esraaalaa

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,26 +1,28 @@
 #include "search_algos.h"
 
 /**
- * binary_search - earches for a value in a sorted array of integers
- *					using the Binary search algorithm
+ * binary_search - earches for a value in a sorted array of integers using the Binary search algorithm
  * @array: array of ints
  * @size: size of array
  * @value: int value we are looking for
  * Return: Always EXIT_SUCCESS
  */
 int binary_search(int *array, size_t size, int value){
-    if (array ==    NULL)
+    if (array == NULL)
         return (-1);
     size_t i, l, h, m;
 
-    l = array[0];
-    h = array[size-1]
-    while(l <= h){
+    l = 0;
+    h = size - 1;
+    while(l <= h)
+    {
         for(i = l; i <= h; i++)
         {
             printf("Searching in array: array[ld]", array[i]);
             if(i != h)
                 printf(", ");
+            else
+            printf('\n');
         }
     mid = l + (h - l) / 2;
     if (array[mid] == value)
@@ -29,10 +31,10 @@ int binary_search(int *array, size_t size, int value){
     {
         h = mid -1;
     }
-    else{
+    else
+    {
         l = mid + 1;
     }
-
     }
     return (-1);
 }
