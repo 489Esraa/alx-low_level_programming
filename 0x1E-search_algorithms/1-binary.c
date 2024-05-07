3822594ba7eb506248e52c4390710109d5ac18e7
489Esraa <asraaalaa2050@gmail.com>
2024-05-07 13:41:10 +0300

    esraaalaa

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -24,7 +24,7 @@ int binary_search(int *array, size_t size, int value){
             else
             printf('\n');
         }
-    mid = l + (h - l) / 2;
+    mid = (h + l) / 2;
     if (array[mid] == value)
         return (mid);
     else if (array[mid] > value)
